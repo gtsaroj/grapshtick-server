2c9901b031b0a448ba76e8d67616d7bd03556f9c
Saroj <sarojgt326@gmail.com>
2024-01-01 16:39:50 +0545

    authentication update

--- config/server.js
+++ config/server.js
@@ -1,10 +1,18 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url : process.env.GOOGLE_REDIRECT_URI,
+  
   app: {
     keys: env.array('APP_KEYS'),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  },
+ google:  {
+   enable: true,
+   key: process.env.GOOGLE_AUTH_CLIENT_ID,
+   secret: process.env.GOOGLE_AUTH_CLIENT_SECRET,
+   redirectUri : process.env.GOOGLE_REDIRECT_URI
   },
 });
